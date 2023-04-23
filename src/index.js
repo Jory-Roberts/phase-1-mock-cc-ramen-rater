b6d3d9002f5384f83a404d768f3ef61b2f606a8e
Jory Roberts <robertsjory13@gmail.com>
2023-04-23 15:44:26 -0500

    adds new ramen

--- src/index.js
+++ src/index.js
@@ -9,24 +9,38 @@ inside the #ramen-detail div and where it says insert-comment-here and insert-ra
 #ramen-menu div. New ramen does not need to persist i.e. disappear after page refresh is OK
 */
 
-document.addEventListener("DOMContentLoaded", () => {});
-
-//console.log(ramenPictures);
-
-const getRamenPictures = () => {
-  return fetch("http://localhost:3000/ramens")
-    .then((res) => res.json())
-    .then((data) => {
-      const ramenMenu = document.querySelector("#ramen-menu");
-      data.forEach((ramen) => {
-        const image = document.createElement("img");
-        image.src = ramen.image;
-        image.classList.add("ramen-picture");
-        ramenMenu.appendChild(image);
+document.addEventListener("DOMContentLoaded", () => {
+  const getRamenPictures = () => {
+    return fetch("http://localhost:3000/ramens")
+      .then((res) => res.json())
+      .then((data) => {
+        const ramenMenu = document.querySelector("#ramen-menu");
+        data.forEach((ramen) => {
+          const image = document.createElement("img");
+          image.src = ramen.image;
+          image.classList.add("ramen-picture");
+          ramenMenu.appendChild(image);
+        });
+        console.log(ramenMenu);
       });
+  };
 
-      console.log(ramenMenu);
-    });
-};
+  const ramenForm = document.querySelector("#new-ramen");
+  ramenForm.addEventListener("submit", createNewRamen);
 
-getRamenPictures();
+  function createNewRamen(e) {
+    e.preventDefault();
+    console.log(ramenForm);
+    const newName = document.querySelector("#new-name").value;
+    console.log(newName);
+    const newRestaurant = document.querySelector("#new-restaurant").value;
+    console.log(newRestaurant);
+    const newImage = document.querySelector("#new-image").value;
+    console.log(newImage);
+    const newRating = document.querySelector("#new-rating").value;
+    console.log(newRating);
+    const newComment = document.querySelector("#new-comment").value;
+    console.log(newComment);
+  }
+  getRamenPictures();
+});
